e65ae7ec1417ea0eb947902231f58b253492d5c0
Angelos <kounavisangelo@gmail.com>
2023-01-09 18:06:21 -0500

    lab 16

--- api/define.py
+++ api/define.py
@@ -9,15 +9,13 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        if "word" in dic:
-            url = "https://restcountries.com/v3.1/capital/{capital}"
-            r = requests.get(url + dic["word"])
+        if "country" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
             data = r.json()
-            definitions = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
-                definitions.append(definition)
-            message = str(definitions)
+            capital = data[0]["capital"][0]
+
+            message = f'the capital of {dic["country"]} is {str(capital)}'
 
         else:
             message = "Give me a word to define please"
